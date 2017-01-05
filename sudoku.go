c9f3ec266e5307dbdf2dece7afe0300157ac96ef
Jon Stites <mail+git@jonstites.com>
2017-01-05 16:47:32 +0000

    create cell constructor

--- sudoku.go
+++ sudoku.go
@@ -5,10 +5,38 @@ import (
 	"strconv"
 )
 
-// Cell represents one square of a Sudoku puzzle.
+// Represents one square of a Sudoku puzzle.
 type Cell struct {
+	// The determined value (meaningless if valueSet is false)
 	value int
+	// Stores which values the cell is allowed to be
+	valueOptions map[int]bool
+	// Whether the value has been set
+	valueSet bool
 }
+
+// Returns the number of values the cell is allowed to be in
+func (c *Cell) numValueOptions() int {
+	num := 0
+	for _, value := range c.valueOptions {
+		if value {
+			num += 1
+		}
+	}
+	return num
+}
+
+// Initialize a cell with possible values 1-9
+func newCell() *Cell {
+	c := Cell{}
+	valueMap := map[int]bool{}
+	for i := 1; i <= 9; i++ {
+		valueMap[i] = true
+	}
+	c.valueOptions = valueMap
+	return &c
+}
+
 
 // Just print the value of a Cell 
 func (c *Cell) String() string {
--- sudoku_test.go
+++ sudoku_test.go
@@ -5,8 +5,33 @@ import (
 )
 
 func TestCellPrint(t *testing.T) {
-	myCell := Cell{3}
+	myCell := Cell{3, map[int]bool{}, true}
 	if !(myCell.String() == "3") {
 		t.Error("Cell{3} should print as 3.")
+	}
+}
+
+func TestCellOptions(t *testing.T) {
+	myCell := Cell{3, map[int]bool{3: true, 5: true}, true}
+	if !(myCell.valueOptions[3]) {
+		t.Error("Cell should be allowed to have value 3")
+	}
+
+}
+
+func TestCellNumOptions(t *testing.T) {
+	myCell := Cell{3, map[int]bool{3: true, 5: true}, true}
+	if (myCell.numValueOptions() != 2) {
+		t.Error("Cell should have 2 options")
+	}
+
+}
+
+func TestCellConstructor(t *testing.T) {
+	myCell := newCell()
+	for i := 1; i <= 9; i++ {
+		if !(myCell.valueOptions[i]) {
+			t.Error("Cell should have options 1-9")
+		}
 	}
 }
