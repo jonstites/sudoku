36fd26132331b1ed19cdd22c4da70234a2925fc3
Jon Stites <mail+git@jonstites.com>
2017-01-07 15:41:41 +0000

    add puzzle class

--- sudoku.go
+++ sudoku.go
@@ -1,18 +1,75 @@
 package sudoku
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
+	"strings"
 )
+
+func Max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+
+
+// Implement our own sets
+type valueSet map[int]bool
+
+// Initialize a valueSet with 1-9
+func newValueSet() valueSet {
+	valueMap := valueSet{}
+	for i := 1; i <= 9; i++ {
+		valueMap[i] = true
+	}
+	return valueMap
+}
+
+// Union of two valueSets
+func union(value1 valueSet, value2 valueSet) valueSet {
+	myValueSet := valueSet{}
+	for key, value := range value1 {
+		if value {
+			myValueSet[key] = value
+		}
+	}
+	for key, value := range value2 {
+		if value {
+			myValueSet[key] = value
+		}
+	}
+	return myValueSet
+}
+
+// Intersection of two valueSets
+func intersection(value1 valueSet, value2 valueSet) valueSet {
+	myValueSet := valueSet{}
+	for key, value := range value1 {
+		if (value && value2[key]) {
+			myValueSet[key] = value
+		}
+	}
+	return myValueSet
+}
 
 // Represents one square of a Sudoku puzzle.
 type Cell struct {
-	// The determined value (meaningless if valueSet is false)
+	// The determined value (meaningless if valueKnown is false)
 	value int
-	// Stores which values the cell is allowed to be
+	// Stores which values the cell is allowed to be (meaningless if valueKnown is true)
 	valueOptions map[int]bool
-	// Whether the value has been set
-	valueSet bool
+	// Whether the value is known
+	valueKnown bool
+}
+
+func newCell() *Cell {
+	cell := new(Cell)
+	cell.valueOptions = newValueSet()
+	return cell
 }
 
 // Returns the number of values the cell is allowed to be in
@@ -26,21 +83,109 @@ func (c *Cell) numValueOptions() int {
 	return num
 }
 
-// Initialize a cell with possible values 1-9
-func newCell() *Cell {
-	c := Cell{}
-	valueMap := map[int]bool{}
-	for i := 1; i <= 9; i++ {
-		valueMap[i] = true
+// Just print the value of a Cell f
+func (c *Cell) String() string {
+	valueString := strconv.Itoa(c.value)
+	if !(c.valueKnown) {
+		valueString = "0"
 	}
-	c.valueOptions = valueMap
-	return &c
+	return valueString
 }
 
+type Puzzle struct {
+	puzzle [][]Cell
+}
 
-// Just print the value of a Cell 
-func (c *Cell) String() string {
-	return strconv.Itoa(c.value)
+func newPuzzle() *Puzzle {
+	myPuzzle := new(Puzzle)
+	myPuzzle.puzzle = make([][]Cell, 9)
+	for i := 0; i < 9; i++ {
+		myPuzzle.puzzle[i] = make([]Cell, 9)
+		for j := 0; j < 9; j++ {
+			myPuzzle.puzzle[i][j] = *newCell()
+		}
+	}
+	return myPuzzle
+}
+
+func (myPuzzle *Puzzle) setValue(rowNum int, colNum int, value int) {
+	myPuzzle.puzzle[rowNum][colNum].value = value
+	myPuzzle.puzzle[rowNum][colNum].valueKnown = true
+}
+
+func (myPuzzle *Puzzle) insertRow(row string, rowNum int) {
+	for colNum, char := range row {
+		value := int(char)
+		myPuzzle.setValue(rowNum, colNum, value)
+	}
+}
+
+func box(cells [][]Cell) string {
+	var box []string
+	for _, row := range cells {
+		var rowValues []string
+		for _, col := range row {
+			rowValues = append(rowValues, col.String())
+		}
+		box = append(box, strings.Join(rowValues, ""))
+	}
+	return strings.Join(box, "\n")
+}
+
+func (myPuzzle *Puzzle) String() string {
+	return box(myPuzzle.puzzle)
+}
+
+func isNumeric(row string) bool {
+	for _, char := range row {
+		if char < '0' || char > '9' {
+			return false
+		}
+	}
+	return true
+}
+
+func validateRowFormat(row string) error {
+	if !(isNumeric(row)) {
+		return fmt.Errorf("File contains non-numeric characters in row: ", row)
+	}
+
+	if len(row) != 9 {
+		return fmt.Errorf("Row does not contain 9 digits: ", row) 
+	}
+
+	return nil
+}
+
+func puzzleFromFile(filename string) *Puzzle {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	myPuzzle := newPuzzle()
+	
+	scanner := bufio.NewScanner(file)
+	i := 0
+	for scanner.Scan() {
+		row := scanner.Text()
+		if i >= 9 {
+			log.Fatal("Files contains more than 9 rows.")
+		}
+		err := validateRowFormat(row)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		myPuzzle.insertRow(row, i)
+		i++
+	}
+	
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return myPuzzle
 }
 
 func main() {
--- sudoku_test.go
+++ sudoku_test.go
@@ -2,12 +2,39 @@ package sudoku
 
 import (
 	"testing"
+	"reflect"
 )
 
+
+func TestUnion(t *testing.T) {
+	value1 := valueSet{1: true, 2:true}
+	value2 := valueSet{1: true, 3:true}
+	value3 := union(value1, value2)
+	if !(reflect.DeepEqual(value3, valueSet{1:true, 2:true, 3:true})) {
+		t.Error("Union of {1,2} and {1,3} should be {1,2,3}")
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	value1 := valueSet{1: true, 2:true}
+	value2 := valueSet{1: true, 3:true}
+	value3 := intersection(value1, value2)
+	if !(reflect.DeepEqual(value3, valueSet{1:true})) {
+		t.Error("Intersection of {1,2} and {1,3} should be {1}")
+	}
+}
+
 func TestCellPrint(t *testing.T) {
-	myCell := Cell{3, map[int]bool{}, true}
+	myCell := Cell{3, valueSet{}, true}
 	if !(myCell.String() == "3") {
 		t.Error("Cell{3} should print as 3.")
+	}
+}
+
+func TestCellPrintEmpty(t *testing.T) {
+	myCell := newCell()
+	if !(myCell.String() == "0") {
+		t.Error("Unknown Cell should print as 0.")
 	}
 }
 
@@ -16,7 +43,6 @@ func TestCellOptions(t *testing.T) {
 	if !(myCell.valueOptions[3]) {
 		t.Error("Cell should be allowed to have value 3")
 	}
-
 }
 
 func TestCellNumOptions(t *testing.T) {
@@ -27,7 +53,7 @@ func TestCellNumOptions(t *testing.T) {
 
 }
 
-func TestCellConstructor(t *testing.T) {
+func TestCellValueOptions(t *testing.T) {
 	myCell := newCell()
 	for i := 1; i <= 9; i++ {
 		if !(myCell.valueOptions[i]) {
@@ -35,3 +61,86 @@ func TestCellConstructor(t *testing.T) {
 		}
 	}
 }
+
+func TestPuzzleNumCellCols(t *testing.T) {
+	myPuzzle := newPuzzle()
+	if (len(myPuzzle.puzzle) != 9) {
+		t.Error("Puzzle should have 9 columns")
+	}
+}
+
+func TestPuzzleNumCellRows(t *testing.T) {
+	myPuzzle := newPuzzle()
+	if (len(myPuzzle.puzzle[0]) != 9) {
+		t.Error("Puzzle should have 9 rows")
+	}
+}
+
+func TestPuzzleCellNumOptions(t *testing.T) {
+	myPuzzle := newPuzzle()
+	numOptions := myPuzzle.puzzle[3][5].numValueOptions()
+	if (numOptions != 9) {
+		t.Error("Puzzle cells should have 9 options, not ", numOptions)
+	}
+}
+
+func TestPuzzleSetValue(t *testing.T) {
+	myPuzzle := newPuzzle()
+	myPuzzle.setValue(3, 2, 8)
+	if (myPuzzle.puzzle[3][2].value != 8) {
+		t.Error("Puzzle should have 8 set at 3,2.")
+	}
+	if (myPuzzle.puzzle[3][2].valueKnown != true) {
+		t.Error("Puzzle should be set at 3,2.")
+	}
+
+}
+
+func TestIsNumeric(t *testing.T) {
+	t1 := "00003897509120398"
+	t2 := "09287308a98098"
+	if !(isNumeric(t1)) {
+		t.Error(t1, " should be marked numeric.")
+	}
+
+	if isNumeric(t2) {
+		t.Error(t2, " should not be marked numeric.")
+	}
+}
+
+func TestValidateRowFormat(t *testing.T) {
+	row1 := "123456788"
+	row2 := "12345678"
+	row3 := "12345678a"
+	row4 := "1234567899"
+
+	if validateRowFormat(row1) != nil {
+ 		t.Error("Row should be valid: ", row1)
+	}
+
+	if validateRowFormat(row2) == nil {
+		t.Error("Row should not be valid: ", row2)
+	}
+
+	if validateRowFormat(row3) == nil {
+		t.Error("Row should not be valid: ", row3)
+	}
+
+	if validateRowFormat(row4) == nil {
+		t.Error("Row should not be valid: ", row4)
+	}
+}
+
+/*
+func TestPuzzleFromFile(t *testing.T) {
+	puzzleOne := newPuzzle()
+	puzzleOne.puzzle[3][2].value = 2
+	puzzleTwo := puzzleFromFile("almost_empty.txt")
+	if !(reflect.DeepEqual(puzzleOne, puzzleTwo)) {
+		t.Error("Puzzles should be equal.")
+	}
+	
+	
+}
+
+*/
